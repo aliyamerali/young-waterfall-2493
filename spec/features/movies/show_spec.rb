abd230a668dfb7da2a3d4bc9864e492f2bc221d0
Aliya Merali <merali4@gmail.com>
2021-06-01 11:03:31 -0600

    Cleanup test setup

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -3,23 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Movies show page' do
   before :each do
     @studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
-    @lost_ark = @studio.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
     @jurassic = @studio.movies.create!(title: 'Jurassic Park', creation_year: 1993, genre: 'Action/Adventure')
-    @shrek = @studio.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy/Fantasy')
-
+    
     @ford = Actor.create!(name:'Harrison Ford', age: 78, currently_working: false)
     @goldblum = Actor.create!(name:'Jeff Goldblum', age: 68, currently_working: true)
     @dern = Actor.create!(name: 'Laura Dern', age: 54, currently_working: true)
     @myers = Actor.create!(name: "Mike Myers", age: 58, currently_working: false)
     @murphy = Actor.create!(name: "Eddie Murphy", age: 60, currently_working: true)
 
-    @shrek.actors << @myers
-    @shrek.actors << @murphy
-    @shrek.actors << @dern
     @jurassic.actors << @goldblum
     @jurassic.actors << @dern
-    @lost_ark.actors << @dern
-    @lost_ark.actors << @ford
 
     visit "/movies/#{@jurassic.id}"
   end
