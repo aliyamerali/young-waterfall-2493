fd316a98e16a5f662ed0e933ca7b12353be52d85
Aliya Merali <merali4@gmail.com>
2021-06-01 10:39:28 -0600

    Add view and update test for us-2

--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -8,19 +8,18 @@ RSpec.describe 'Studios show page' do
     @shrek = @studio.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy/Fantasy')
 
     @ford = Actor.create!(name:'Harrison Ford', age: 78, currently_working: false)
-    @lost_ark.actors << @ford
-
     @goldblum = Actor.create!(name:'Jeff Goldblum', age: 68, currently_working: true)
     @dern = Actor.create!(name: 'Laura Dern', age: 54, currently_working: true)
+    @myers = Actor.create!(name: "Mike Myers", age: 58, currently_working: false)
+    @murphy = Actor.create!(name: "Eddie Murphy", age: 60, currently_working: true)
+
+    @shrek.actors << @myers
+    @shrek.actors << @murphy
+    @shrek.actors << @dern
     @jurassic.actors << @goldblum
     @jurassic.actors << @dern
     @lost_ark.actors << @dern
-    @shrek.actors << @dern
-
-    @myers = Actor.create!(name: "Mike Myers", age: 58, currently_working: false)
-    @murphy = Actor.create!(name: "Eddie Murphy", age: 60, currently_working: true)
-    @shrek.actors << @myers
-    @shrek.actors << @murphy
+    @lost_ark.actors << @ford
 
     visit "/studios/#{@studio.id}"
   end
@@ -36,8 +35,6 @@ RSpec.describe 'Studios show page' do
     expect(page).to have_content(@shrek.title)
   end
 
-  # I see a list of actors that have acted in any of the studio's movies
-  # And I see that the list of actors only includes actors that are currently working
   it 'shows a list of *currently working* actors in the studios movies' do
     expect(page).to have_content(@goldblum.name)
     expect(page).to have_content(@dern.name)
@@ -46,15 +43,13 @@ RSpec.describe 'Studios show page' do
     expect(page).to_not have_content(@myers.name)
   end
 
-  # And I see that the list of actors is unique (no duplicate actors)
   it 'does not show duplicate actors' do
     expect(page).to have_content(@dern.name, count: 1)
   end
 
-  # And I see that the list of actors is ordered from oldest actor to youngest
   it 'orders actors from oldest to youngest' do
-    expect(@goldblum).to appear_before(@murphy)
-    expect(@murphy).to appear_before(@dern)
+    expect(@goldblum.name).to appear_before(@murphy.name)
+    expect(@murphy.name).to appear_before(@dern.name)
   end
 
 
