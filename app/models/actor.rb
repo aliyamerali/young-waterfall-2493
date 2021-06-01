fd3662bb39b8a48c5721d6e07d4646e0855812e8
Aliya Merali <merali4@gmail.com>
2021-06-01 11:40:44 -0600

    Add model method for retrieving coactors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,14 @@ class Actor < ApplicationRecord
   has_many :castings
   has_many :movies, through: :castings
 
+  def coactors
+    movie_ids = movies.pluck(:id)
+    Movie.joins(:actors)
+         .select('actors.name')
+         .where('movies.id' => movie_ids)
+         .where.not("actors.id = #{self.id}")
+         .distinct
+         .order('actors.name')
+  end
+
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,33 @@ RSpec.describe Actor do
     it {should have_many :castings}
     it {should have_many(:movies).through(:castings)}
   end
+
+  before :each do
+    @studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    @lost_ark = @studio.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    @jurassic = @studio.movies.create!(title: 'Jurassic Park', creation_year: 1993, genre: 'Action/Adventure')
+    @shrek = @studio.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy/Fantasy')
+
+    @ford = Actor.create!(name:'Harrison Ford', age: 78, currently_working: false)
+    @goldblum = Actor.create!(name:'Jeff Goldblum', age: 68, currently_working: true)
+    @dern = Actor.create!(name: 'Laura Dern', age: 54, currently_working: true)
+    @myers = Actor.create!(name: "Mike Myers", age: 58, currently_working: false)
+    @murphy = Actor.create!(name: "Eddie Murphy", age: 60, currently_working: true)
+
+    @jurassic.actors << @dern
+    @lost_ark.actors << @dern
+    @shrek.actors << @myers
+    @shrek.actors << @murphy
+    @jurassic.actors << @goldblum
+    @lost_ark.actors << @ford
+  end
+
+
+  describe 'instance methods' do
+    it '#coactors returns a list of actors an actor has worked with' do
+      expect(@dern.coactors.length).to eq(2)
+      expect(@dern.coactors.first.name).to eq(@ford.name)
+      expect(@dern.coactors.last.name).to eq(@goldblum.name)
+    end
+  end
 end
